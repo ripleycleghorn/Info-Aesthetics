c7958537f45687816f8f5bee2e2f133153524b3b
Ripley Cleghorn <ripley.cleghorn@gmail.com>
2020-10-23 11:22:37 -0300

    updated d3 and implemented highcharts

---
+++ assignment3/graph.js
@@ -0,0 +1,83 @@
+//create margins and dimensions
+const margin = {top: 20, bottom: 0, left: 150, right: 100}
+const graphWidth = 800 - margin.left - margin.right;
+const graphHeight= 600 - margin.top - margin.bottom;
+
+const svg = d3.select('.canvas')
+    .append('svg')
+        .attr('width', 800)
+        .attr('height', 650)
+        .style("background-color", "white")
+        .style("margin", "auto")
+    .append('g')
+        .attr('width', graphWidth)
+        .attr('height', graphHeight)
+        .attr('transform', `translate(${margin.left}, ${margin.top})`)
+
+
+d3.json("data.json").then(data => {
+
+    //sort bars based on value
+    data = data.sort(function (a, b) {
+        return d3.ascending(b.value, a.value);
+    })
+
+    // Add X axis
+    var x = d3.scaleLinear()
+        .domain([0,d3.max(data, d => d.value/13)])
+        .range([ 0, 580]);
+    svg.append("g")
+        .attr("transform", "translate(0," + graphHeight + ")")
+        .call(d3.axisBottom(x).ticks(5))
+        .selectAll("text")
+        // .attr("transform", "translate(-10,0)rotate(-45)")
+        .style("text-anchor", "middle")
+        .style("font-family", "'Verdana', sans-serif")
+        .style("color", "#555")
+        .style("font-size", 12);
+
+    // Add X axis label:
+    svg.append("text")
+        .attr("text-anchor", "end")
+        .attr("x", 350)
+        .attr("y", graphHeight + 40)
+        .text("Percent of respondents")
+        .style("font-family", "'Verdana', sans-serif")
+        .style("color", "#555")
+        .style("font-size", 12);
+
+    // Y axis
+    var y = d3.scaleBand()
+    .domain(data.map(function(d) { return d.reason; }))
+    .range([ 0, 580 ])
+    .padding(1);
+    svg.append("g")
+        .call(d3.axisLeft(y))
+        .selectAll("text")
+        .style("font-family", "'Verdana', sans-serif")
+        .style("color", "#555")
+        .style("font-size", 12);
+
+    // Lines
+    svg.selectAll("myline")
+    .data(data)
+    .enter()
+    .append("line")
+        .attr("x1", function(d) { return x(d.value/13); })
+        .attr("x2", x(0))
+        .attr("y1", function(d) { return y(d.reason); })
+        .attr("y2", function(d) { return y(d.reason); })
+        .attr("stroke", "grey")
+        .attr("stroke-width", "2")
+
+    // Circles
+    svg.selectAll("mycircle")
+    .data(data)
+    .enter()
+    .append("circle")
+        .attr("cx", function(d) { return x(d.value/13); })
+        .attr("cy", function(d) { return y(d.reason); })
+        .attr("r", "6")
+        .style("fill", "#829FD9")
+        // .attr("stroke", "black")
+    })
---
+++ test/app.js
@@ -0,0 +1,35 @@
+Highcharts.chart('container', {
+    chart: {
+        type: 'bar'
+    },
+    title: {
+        text: 'Reasons for Not Wanting to Vote'
+    },
+    xAxis: {
+        categories: ['I’m not interested in what is happening in government.','I just don’t think about doing it.','Voting has little to do with the way real decisions are made. ', 'I just don’t think about doing it.', 'I don’t see a difference between the candidates or parties.','I don’t like any of the candidates on the ballot.','I’m not informed enough about the candidates or issues to make a good decision. ','I dislike politics.','My one vote isn’t going to affect how things turn out. ','I have been unable to vote due to a disability or language barrier. ','I am too busy to vote.','My religion discourages voting. ']
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Number of Respondents'
+        }
+    },
+    legend: {
+        reversed: true
+    },
+    plotOptions: {
+        series: {
+            stacking: 'normal'
+        }
+    },
+    series: [{
+        name: 'Not',
+        data: [298,335,227,335,294,265,337,193,246,505,434,461]
+    }, {
+        name: 'Major',
+        data: [104,97,201,97,137,177,94,227,167,19,33,78]
+    }, {
+        name: 'Minor',
+        data: [161,131,134,131,130,120,132,143,149,36,95,23]
+    }]
+});
